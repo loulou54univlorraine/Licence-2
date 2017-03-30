81d149bf1c5bbc009aedaebba9669aa4f49f2e3d
Drizzik <gauthier.heyob@gmail.com>
2017-03-30 12:00:31 +0200

    TP BPO jeudemot part 3-1

--- BPO TP Jeu de mots/src/projetBPO/Test.java
+++ BPO TP Jeu de mots/src/projetBPO/Test.java
@@ -4,6 +4,8 @@ package projetBPO;
 import projetBPO.jeux.Dictionnaire;
 import projetBPO.jeux.Etat;
 import projetBPO.jeux.JeuDeMots;
+import projetBPO.jeux.algos.Historique;
+import projetBPO.jeux.algos.LargeurDAbord;
 import projetBPO.jeux.algos.ProfondeurDAbord;
 
 import java.io.IOException;
@@ -16,13 +18,41 @@ public class Test {
         Dictionnaire dico = Dictionnaire.getInstance();
         dico.setMots("Dictionnaires/dico4.txt");
         ProfondeurDAbord profD= new ProfondeurDAbord();
+        LargeurDAbord larD= new LargeurDAbord();
         Etat jdm = new JeuDeMots("lion");
         jdm.setFinal(new JeuDeMots("peur"));
-        if(profD.existeChemin(jdm)){
-            System.out.println("il existe un chemin");
-        }else{
-            System.out.println("Pas de chemin");
-        }
 
+
+        Historique h = new Historique();
+        JeuDeMots j1 = new JeuDeMots("a");
+        JeuDeMots j2= new JeuDeMots("a");
+        h.ajouter(j1);
+        assert h.contient(j2):"histo faux";
+
+
+        long startTimeProfD = System.nanoTime();
+        if(profD.existeChemin(jdm)){
+            System.out.println("profD :Il existe un chemin");
+        }else{
+            System.out.println("profD :Pas de chemin");
+        }
+        long estimatedTimeprofD = System.nanoTime() - startTimeProfD;
+        System.out.println("\nTemps avec profD :"+estimatedTimeprofD+"\n");
+        System.out.println("---------------------------------------------");
+        long startTimeLarD = System.nanoTime();
+        if(larD.existeChemin(jdm)){
+            System.out.println("larD :Il existe un chemin");
+        }else{
+            System.out.println("larD :Pas de chemin");
+        }
+        long estimatedTimeLarD = System.nanoTime() - startTimeLarD;
+        System.out.println("\nTemps avec larD :"+ estimatedTimeLarD+"\n");
+        System.out.println("---------------------------------------------");
+
+        if(estimatedTimeprofD<estimatedTimeLarD){
+            System.out.println("Le plus rapide est le ProfD");
+        }else{
+            System.out.println("Le plus rapide est le LarD");
+        }
     }
 }
--- BPO TP Jeu de mots/src/projetBPO/jeux/algos/LargeurDAbord.java
+++ BPO TP Jeu de mots/src/projetBPO/jeux/algos/LargeurDAbord.java
@@ -2,6 +2,11 @@ package projetBPO.jeux.algos;
 
 import projetBPO.jeux.Etat;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by Gauthier on 30/03/2017.
  */
@@ -15,7 +20,25 @@ public class LargeurDAbord implements IRecherche {
     }
 
     public boolean existeChemin(Etat etat, Historique h){
-        return true;
+        if (etat.estFinal()) {
+            return true;
+        }
+        Queue<Etat> queue = new LinkedList();
+        queue.add(etat);
+        boolean trouve = false;
+        while (!queue.isEmpty()&& !trouve){
+            queue.poll();
+            Iterator<Etat> ite= etat.iterator();
+            while (ite.hasNext()&& !trouve){
+                Etat e=ite.next();
+                if(!h.contient(e)){
+                    h.ajouter(e);
+                    trouve=existeChemin(e,h);
+                }
+
+            }
+        }
+        return trouve;
     }
 
 
